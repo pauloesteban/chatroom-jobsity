30c2f2a70e43de37ca5a3a68178425616936ad49
Paulo Chiliguano <paulo.chiliguano@gmail.com>
2019-10-05 14:52:16 -0500

    Stock parser function

--- stooqparser.py
+++ stooqparser.py
@@ -1,8 +1,6 @@
 import csv
 import urllib.request
 
-stock_code = 'aapl.us'
-
 def stock_share_parser(stock_code):
     target_url = 'https://stooq.com/q/l/?s=' + stock_code + '&f=sd2t2ohlcv&h&e=csv'
     response = urllib.request.urlopen(target_url)
@@ -13,4 +11,8 @@ def stock_share_parser(stock_code):
         if line_count == 1:
             stock_quote_message = row[0] + " quote is $" + row[-2] + " per share"
             return stock_quote_message
-    line_count += 1
+        line_count += 1
+
+stock_code_apple= 'aapl.us'
+test = stock_share_parser(stock_code_apple)
+print(test)
